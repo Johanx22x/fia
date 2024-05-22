31c233d4b0d49db4925c3e3578dd96184eced5dc
Johanx22x <johanrodsa2210@gmail.com>
2024-05-22 14:29:31 -0600

    add doc to generador

--- generador/patron_generador.py
+++ generador/patron_generador.py
@@ -7,13 +7,16 @@ class Visitante:
 
     tabuladores = 0
     
-    def retornar_tabuladores(self):
+    def retornar_tabuladores(self) -> str:
+        """Retorna una cadena con la cantidad de tabuladores necesarios."""
         return " " * self.tabuladores
 
-    def incrementar_tabuladores(self):
+    def incrementar_tabuladores(self) -> None:
+        """Incrementa la cantidad de tabuladores."""
         self.tabuladores += 1
 
-    def decrementar_tabuladores(self):
+    def decrementar_tabuladores(self) -> None:
+        """Decrementa la cantidad de tabuladores."""
         self.tabuladores -= 1
 
     def visitar(self, nodo : TipoNodo) -> str:
@@ -21,13 +24,15 @@ class Visitante:
         visitante = getattr(self, f'visitar_{nodo.tipo.name.lower()}')
         return visitante(nodo)
 
-    def visitar_campeonato(self, nodo):
+    def visitar_campeonato(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo campeonato."""
         instrucciones = []
         for hijo in nodo.hijos:
             instrucciones.append(self.visitar(hijo))
         return '\n'.join(instrucciones)
 
-    def visitar_escuderia(self, nodo):
+    def visitar_escuderia(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo escuderia."""
         instrucciones = []
         self.incrementar_tabuladores()
         for hijo in nodo.hijos:
@@ -35,37 +40,46 @@ class Visitante:
         self.decrementar_tabuladores()
         return 'escuderia ' + nodo.lexema + ' {\n' + '\n'.join(instrucciones) + '\n' + self.retornar_tabuladores() + '}'
 
-    def visitar_miembros(self, nodo):
+    def visitar_miembros(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo miembros."""
         instrucciones = []
         for hijo in nodo.hijos:
             instrucciones.append(self.visitar(hijo))
         return '\n'.join(instrucciones)
     
-    def visitar_piloto(self, nodo):
+    def visitar_piloto(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo piloto."""
         return f'piloto {nodo.lexema} {"".join(self.visitar(hijo) for hijo in nodo.hijos)};'
     
-    def visitar_director(self, nodo):
+    def visitar_director(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo director."""
         return f'director {nodo.lexema} {"".join(self.visitar(hijo) for hijo in nodo.hijos)};'
     
-    def visitar_ingeniero(self, nodo):
+    def visitar_ingeniero(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo ingeniero."""
         return f'ingeniero {nodo.lexema} {nodo.hijos[0]};'
     
-    def visitar_recursos(self, nodo):
+    def visitar_recursos(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo recursos."""
         instrucciones = []
         for hijo in nodo.hijos:
             instrucciones.append(self.visitar(hijo))
         return '\n'.join(instrucciones)
 
-    def visitar_presupuesto(self, nodo):
+    def visitar_presupuesto(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo presupuesto."""
         return f'presupuesto {nodo.lexema};'
 
-    def visitar_capital(self, nodo):
+    def visitar_capital(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo capital."""
         return f'capital {nodo.lexema};'
 
-    def visitar_auto(self, nodo):
+    def visitar_auto(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo auto."""
         return f'auto {nodo.lexema} {"".join(self.visitar(hijo) for hijo in nodo.hijos)};'
 
-    def visitar_funcion(self, nodo):
+    def visitar_funcion(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo función."""
         instrucciones = []
         self.incrementar_tabuladores()
         for hijo in nodo.hijos:
@@ -73,29 +87,36 @@ class Visitante:
         self.decrementar_tabuladores()
         return 'orden ' + nodo.lexema + ' (' + self.visitar(nodo.hijos[0]) + ') {\n' + '\n'.join(instrucciones[1:]) + '\n' + self.retornar_tabuladores() + '}'
  
-    def visitar_llamada_funcion(self, nodo):
+    def visitar_llamada_funcion(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo llamada_función."""
         argumentos = ', '.join(self.visitar(hijo) for hijo in nodo.hijos)
         return f'{nodo.lexema}({argumentos});'
 
-    def visitar_parametros(self, nodo):
+    def visitar_parametros(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo parámetros."""
         parametros = ', '.join(self.visitar(hijo) for hijo in nodo.hijos)
         return parametros
     
-    def visitar_parametro(self, nodo):
+    def visitar_parametro(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo parámetro."""
         return f'{self.visitar(nodo.hijos[0])} {nodo.lexema}'
     
-    def visitar_tipo_dato(self, nodo):
+    def visitar_tipo_dato(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo tipo_dato."""
         return nodo.lexema
 
-    def visitar_variable(self, nodo):
+    def visitar_variable(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo variable."""
         if len(nodo.hijos) > 0:
             return f"{nodo.lexema} {self.visitar(nodo.hijos[0])} {self.visitar(nodo.hijos[1])}"
         return f'{nodo.lexema}'
 
-    def visitar_retorno(self, nodo):
+    def visitar_retorno(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo retorno."""
         return f'confirmacion {self.visitar(nodo.hijos[0])}'
 
-    def visitar_instrucciones(self, nodo):
+    def visitar_instrucciones(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo instrucciones."""
         instrucciones = []
         self.incrementar_tabuladores()
         for hijo in nodo.hijos:
@@ -103,7 +124,8 @@ class Visitante:
         self.decrementar_tabuladores()
         return '\n'.join(instrucciones)
 
-    def visitar_condiciones(self, nodo):
+    def visitar_condiciones(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo condiciones."""
         instrucciones = []
         self.incrementar_tabuladores()
         for hijo in nodo.hijos:
@@ -111,7 +133,8 @@ class Visitante:
         self.decrementar_tabuladores()
         return 'box (' + self.visitar(nodo.hijos[0]) + ') {\n' + '\n'.join(instrucciones[1:]) + '\n' + self.retornar_tabuladores() + '}'
     
-    def visitar_condiciones_out(self, nodo):
+    def visitar_condiciones_out(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo condiciones_out."""
         instrucciones = []
         self.incrementar_tabuladores()
         for hijo in nodo.hijos:
@@ -119,7 +142,8 @@ class Visitante:
         self.decrementar_tabuladores()
         return '} out {\n' + '\n'.join(instrucciones) 
 
-    def visitar_ciclos(self, nodo):
+    def visitar_ciclos(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo ciclos."""
         instrucciones = []
         self.incrementar_tabuladores()
         for hijo in nodo.hijos:
@@ -127,38 +151,48 @@ class Visitante:
         self.decrementar_tabuladores()
         return 'circuito (' + self.visitar(nodo.hijos[0]) + ') {\n' + '\n'.join(instrucciones[1:]) + '\n' + self.retornar_tabuladores() + '}'  
 
-    def visitar_instruccion(self, nodo):
+    def visitar_instruccion(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo instrucción."""
         instrucciones = []
         for hijo in nodo.hijos:
             instrucciones.append(self.visitar(hijo))
         return '\n'.join(instrucciones)
 
-    def visitar_declaracion_variable(self, nodo):
+    def visitar_declaracion_variable(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo declaración_variable."""
         return f'{self.visitar(nodo.hijos[0])} {self.visitar(nodo.hijos[1])};'
 
-    def visitar_asignacion(self, nodo):
+    def visitar_asignacion(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo asignación."""
         return f'{self.visitar(nodo.hijos[0])} = {self.visitar(nodo.hijos[1])}'
     
-    def visitar_expresion(self, nodo):
+    def visitar_expresion(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo expresión."""
         if len(nodo.hijos) > 0:
             return f"{nodo.lexema} {self.visitar(nodo.hijos[0])} {self.visitar(nodo.hijos[1])};"
         return f'{nodo.lexema}'
 
-    def visitar_operador(self, nodo):
+    def visitar_operador(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo operador."""
         return f"{nodo.lexema}"
 
-    def visitar_argumentos(self, nodo):
+    def visitar_argumentos(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo argumentos."""
         return ', '.join(self.visitar(hijo) for hijo in nodo.hijos)
 
-    def visitar_agrupacion(self, nodo):
+    def visitar_agrupacion(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo agrupación."""
         return f'({self.visitar(nodo.hijos[0])})'
     
-    def visitar_acceso_lista(self, nodo):
+    def visitar_acceso_lista(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo acceso_lista."""
         hijos_visitados = [self.visitar(hijo) for hijo in nodo.hijos]
         return f'{nodo.lexema}[{", ".join(hijos_visitados)}]'
 
-    def visitar_valor(self, nodo):
+    def visitar_valor(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo valor."""
         return f' {nodo.lexema}'
     
-    def visitar_tipo_ingeniero(self, nodo):
+    def visitar_tipo_ingeniero(self, nodo: Nodo) -> str:
+        """Visita un nodo de tipo tipo_ingeniero."""
         return f' {nodo.lexema}'
