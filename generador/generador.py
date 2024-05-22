913ed19ec0bec6af6fd1d908c49de88957a05cad
Johanx22x <johanrodsa2210@gmail.com>
2024-05-22 14:08:08 -0600

    merge

--- generador/generador.py
+++ generador/generador.py
@@ -2,24 +2,19 @@
 from generador.patronGenerador import Visitante
 
 class Generador:
-
-    visitador  : Visitante
-
     """Generador de codigo Python"""
+
     def __init__(self, arbol: Visitante) -> None:
         """Inicializa el generador"""
         self.arbol = arbol
         self.visitador = Visitante()
-        ##self.visitador = Nodo('tipo', 'contenidobuenas')
 
     def imprimir(self, nivel=0):
+        """Imprime el arbol"""
         self.arbol.imprimir(nivel)
 
     def generar(self) -> str:
         """Genera codigo Python"""
         self.imprimir()
         resultado = self.visitador.visitar(self.arbol)
-        print()
-        print(resultado)
         return resultado
- 
--- main.py
+++ main.py
@@ -10,32 +10,33 @@ def init():
 
 def main():
     # Obtener el código fuente del archivo
-    with open("./gramatica/ejercicio12.fia", "r") as file:
+    with open("./gramatica/ejercicio11.fia", "r") as file:
         codigo_fuente = file.read()
 
     # Tokenizar el código fuente
     explorador = Explorador(codigo_fuente)
     tokens = explorador.tokenizar()
 
-    #for token in tokens:
-    #    print(token)
+    for token in tokens:
+        print(token)
 
-    #print()
-    #print()
+    print()
+    print()
 
     # Analizar los tokens
     analizador = Analizador(tokens)
     arbol = analizador.analizar()
-    #arbol.imprimir()
+    arbol.imprimir()
 
-    # TODO: Verificación del árbol
+    # Verificar la semántica del código
+    # verificador = Verificador(arbol)
+    # verificador.verificar()
+    # print(verificador.tabla_simbolos)
 
-    # TODO: Generación de código Python
-    
     generador = Generador(arbol)
-    #codigo_python = generador.generar()
+    codigo_python = generador.generar()
     generador.generar()
-    #print(codigo_python)
+    print(codigo_python)
 
 
 if __name__ == "__main__":
